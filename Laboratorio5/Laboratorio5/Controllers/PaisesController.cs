6d23d450b2bb3f24487c6f24edf396a6de7d9719
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-05-19 19:00:47 -0600

    Add controller for Paises

---
+++ Laboratorio5/Laboratorio5/Controllers/PaisesController.cs
@@ -0,0 +1,18 @@
+﻿using Laboratorio5.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Laboratorio5.Controllers
+{
+    public class PaisesController : Controller
+    {
+        public IActionResult Index()
+        {
+            PaisesHandler paisesHandler = new PaisesHandler();
+            var paises = paisesHandler.ObtenerPaises();
+            ViewBag.MainTitle = "Lista de países";
+            return View(paises);
+        }
+    }
+}
