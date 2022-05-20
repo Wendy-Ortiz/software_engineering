bd83bd8717c284c5e2569537ae536484b99042ab
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-05-19 19:01:42 -0600

    Added Model for pais

---
+++ Laboratorio5/Laboratorio5/Models/PaisModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Laboratorio5.Models
+{
+    public class PaisModel
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Continente { get; set; }
+        public string Idioma { get; set; }
+    }
+}
