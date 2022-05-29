276ad91c538893e166cfe188fbb6e00f06b140cb
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-05-28 23:46:02 -0600

    Create required attributes and error messages in Model for lab#6

--- Laboratorio5/Laboratorio5/Models/PaisModel.cs
+++ Laboratorio5/Laboratorio5/Models/PaisModel.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations; //permiten utilizar los elementos Requiered
+
 namespace Laboratorio5.Models
 {
     public class PaisModel
     {
         public int Id { get; set; }
+
+        [Required (ErrorMessage = " Debe proporcionar un nombre" )]
+        //sirven para hacer el binding de las propiedades del modelo en la vista 
+        [DisplayName(" Nombre del país : ")]
         public string Nombre { get; set; }
+        [Required(ErrorMessage = "Debe seleccionar un continente")]
+        [DisplayName(" Continente : " )]
         public string Continente { get; set; }
+        [Required(ErrorMessage = " Debe ingresar el idioma ")]
+        [DisplayName(" Idioma : ")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage= " No puede ingresar numeros " ) ]
         public string Idioma { get; set; }
     }
 }
