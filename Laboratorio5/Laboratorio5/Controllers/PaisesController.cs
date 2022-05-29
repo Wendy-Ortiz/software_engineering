a59c20fd8af71d7fbbf6797fb70a9d156c7c994b
Wendy-Ortiz <wortizalfaro281@gmail.com>
2022-05-28 23:41:29 -0600

    Create controller methods for create, modify and delete a country in lab #6

--- Laboratorio5/Laboratorio5/Controllers/PaisesController.cs
+++ Laboratorio5/Laboratorio5/Controllers/PaisesController.cs
@@ -1,4 +1,5 @@
 ﻿using Laboratorio5.Handlers;
+using Laboratorio5.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,119 @@ namespace Laboratorio5.Controllers
             var paises = paisesHandler.ObtenerPaises();
             ViewBag.MainTitle = "Lista de países";
             return View(paises);
+        }
+        [HttpGet]
+        public ActionResult CrearPais()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CrearPais(PaisModel pais)
+        {
+            ViewBag.ExtoAlCrear = false;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    PaisesHandler paisesHandler = new PaisesHandler();
+                    ViewBag.ExitoAlCrear = paisesHandler.CrearPais(pais);
+
+                    if (ViewBag.ExitoAlCrear)
+                    {
+                        ViewBag.Message = "El pais" + " " + pais.Nombre + " fue Creado con éxito";
+                        ModelState.Clear();
+                    }
+                }
+                return View();
+            }
+            catch
+            {
+                ViewBag.Message = "Error al crear el país";
+                return View();
+            }
+        }
+
+        [HttpGet] //encargada de ejecutar la vista que contiene el formulario
+        public ActionResult EditarPais(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                var paisesHandler = new PaisesHandler();
+                var pais = paisesHandler.ObtenerPaises().Find(model => model.Id == identificador);
+                if (pais == null)
+                {
+                    vista = RedirectToAction("Index");
+
+                }
+                else
+                {
+                    vista = View(pais);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("Index");
+            }
+            return vista;
+        }
+
+        [HttpPost] //método encargado de llamar al handler para
+                   //crear las modificaciones en la base de datos
+        public ActionResult EditarPais(PaisModel pais)
+        {
+            try
+            {
+                var paisesHandler = new PaisesHandler();
+                paisesHandler.EditarPais(pais);
+                return RedirectToAction("Index", "Paises");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpPost] //método encargado de llamar al handler para
+                   //crear las modificaciones en la base de datos
+        public ActionResult BorrarPais(PaisModel pais)
+        {
+            try
+            {
+                var paisesHandler = new PaisesHandler();
+                paisesHandler.BorrarPais(pais);
+                return RedirectToAction("Index", "Paises");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpGet] //encargada de ejecutar la vista que contiene el formulario
+        public ActionResult BorrarPais(int? identificador)
+        {
+            ActionResult vista;
+            try
+            {
+                var paisesHandler = new PaisesHandler();
+                var pais = paisesHandler.ObtenerPaises().Find(model => model.Id == identificador);
+                if (pais == null)
+                {
+                    vista = RedirectToAction("Index");
+
+                }
+                else
+                {
+                    vista = View(pais);
+                }
+            }
+            catch
+            {
+                vista = RedirectToAction("Index");
+            }
+            return vista;
         }
     }
 }
